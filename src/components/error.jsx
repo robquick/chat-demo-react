4433e220f8285e3f0e2a9b73d3009de8bc4283f4
Rob Quick <rob.quick@gmail.com>
2016-04-10 18:49:08 -0400

    added stub 404 page

---
+++ src/components/error.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+let Error = () => {
+    return (
+      <div>WAT?????</div>  
+    );
+};
+
+export default Error;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import {Route, IndexRoute} from "react-router";
 import App from "../components/app.jsx";
 import ChatRoom from "../components/chatroom.jsx";
+import Error from "../components/error.jsx";
 
 let routes = (
   <Route path="/" component={App}>
     <IndexRoute component={ChatRoom} />
+    <Route path="*" component={Error} />
   </Route>
 );
 
