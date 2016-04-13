161a3cc26f3ed7e6a5990442ec03b33231449f1c
Rob Quick <rob.quick@gmail.com>
2016-04-13 01:06:00 -0400

    implemented some basic actions with firebase/thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,51 @@
+import Firebase from "firebase";
+let firebaseRef = Firebase("URL HERE");
+
 export const SEND_MESSAGE = "SEND_MESSAGE";
 export const LOGIN_USER = "LOGIN_USER";
+export const RECEIVED_USERS = "RECEIVED_USERS";
+export const RECEIVED_MESSAGE = "RECEIVED_MESSAGE";
 
 export function sendMessage(text) {
     console.log(text);
-    return {
-        type: SEND_MESSAGE,
-        text
-    };
+    return (dispatch, getState) => {
+        var userName = getState().currentUser.name;
+        firebaseRef.child("messages").push({ userName, text }).then(function() {
+            dispatch({ type: SEND_MESSAGE, userName, text });
+        });
+    }
 }
 
 export function loginUser(name) {
     console.log(name);
-    return {
-        type: LOGIN_USER,
-        name
+    return dispatch => {
+        firebaseRef.child("users").push({ name }).then(function() {
+            dispatch({
+                type: LOGIN_USER,
+                name
+            });
+        });
+    }
+}
+
+export function startListeningForUsers() {
+    return dispatch => {
+        firebaseRef.child("users").on("value", function(snapshot) {
+            dispatch({
+               type: RECEIVED_USERS,
+               data: snapshot.val()
+            });
+        });
+    }
+}
+
+export function startListeningForMessages() {
+    return dispatch => {
+        firebaseRef.child("messages").on("child_added", function(snapshot) {
+            dispatch({
+               type: RECEIVED_MESSAGE,
+               message: snapshot.val()
+            });
+        });
     }
 }
