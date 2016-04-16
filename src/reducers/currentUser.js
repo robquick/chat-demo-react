df798883bdb83d48b751fca279d170a8b65445d6
Rob Quick <rob.quick@gmail.com>
2016-04-16 18:45:15 -0400

    added currentUser reducer

---
+++ src/reducers/currentUser.js
@@ -0,0 +1,13 @@
+import Immutable from "immutable";
+import * as types from "../actions";
+
+const currentUser = (state, action) => {
+    switch(action.type) {
+        case types.SIGN_IN_USER: {
+            state = Immutable.fromJS(action.user);
+        }
+    }
+    return state;
+}
+
+export default currentUser;
