95ad1737c6c61089bf4d4299d854227a6f338361
Rob Quick <rob.quick@gmail.com>
2016-04-16 23:18:50 -0400

    scroll message list when new message is added

--- src/components/messagelist.jsx
+++ src/components/messagelist.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Message from "./message.jsx";
 
-let MessageList = (props) => {
-  let messageElems = props.messages.map(m => <Message message={m} />)
-  return (
-      <div>{messageElems}</div>
-  );  
-};
+class MessageList extends React.Component {
+    componentWillUpdate() {
+        const d = this.div;
+        this.shouldScrollBottom = d.scrollTop + d.offsetHeight === d.scrollHeight;
+    }   
+    
+    componentDidUpdate() {
+        if (this.shouldScrollBottom) {
+            this.div.scrollTop = this.div.scrollHeight;
+        }
+    }
+     
+    render(props) {
+        let messageElems = props.messages.map(m => <Message message={m} />)
+        return (
+            <div ref={e => this.div = e}>{messageElems}</div>
+        );  
+    }
+}
 
 MessageList.propTypes = {
     messages: React.PropTypes.array.isRequired
