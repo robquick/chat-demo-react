7f27c8002b444011abfe6767f197142d416a3ded
Rob Quick <rob.quick@gmail.com>
2016-04-17 17:51:11 -0400

    improved look of messages

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+const msgStyles = { fontSize: "large", border: "1px solid #e7e7e7" };
+const nameStyles = { fontWeight: "bold", paddingTop: 5, paddingLeft: 10 };
+const textStyles = { paddingBottom: 5, paddingLeft: 10 };
+
 let Message = (props) => {
-  let msg = props.message;
+  const msg = props.message;
   return (
-      <div>{msg.get("userName")} > {msg.get("text")}</div>
+      <div style={msgStyles}>
+        <div style={nameStyles}>{msg.get("userName")}</div>
+        <div style={textStyles}>{msg.get("text")}</div>
+      </div>
   );  
 };
 
