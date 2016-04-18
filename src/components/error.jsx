0071a73577686ee2ac21ea7c073c461bb2fbf9f5
Rob Quick <rob.quick@gmail.com>
2016-04-17 23:47:50 -0400

    updated look of 404 page

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router";
+
+const styles = { width: "45%", paddingTop: 10 };
 
 let Error = () => {
     return (
-      <div>WAT?????</div>  
+      <div className="container-fluid" style={styles}>
+        <div className="jumbotron">
+          <h1>404:  OH NOES!</h1>
+          <p>Are you lost?</p>
+          <p><Link to="/">Return Home</Link></p>
+        </div>
+      </div>  
     );
 };
 
