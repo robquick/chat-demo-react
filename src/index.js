ce0399f9e2e634843cfe2e799d3b107f226445d6
Rob Quick <rob.quick@gmail.com>
2018-03-10 11:51:07 -0500

    fixed last warning - import css without storing in var
    Also realized I can import the unminified version, and less the “build”
    script minify for production builds

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import * as actions from "./actions";
 import AppContainer from "./containers/appcontainer";
-import boostrapCSS from "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 const reactElem = (
     <BrowserRouter>
